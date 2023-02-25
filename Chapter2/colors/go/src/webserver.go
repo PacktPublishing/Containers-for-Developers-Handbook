299b806f713b5891d9bce8033c85b729d0512618
Javier Ramirez <frjaraur@gmail.com>
2023-02-25 12:45:46 +0100

    Added golang code

---
+++ Chapter2/colors/go/src/webserver.go
@@ -0,0 +1,16 @@
+cat src/webserver.go
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":3000", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
